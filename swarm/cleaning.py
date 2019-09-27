da857acb3da43420da6d4b71ae50c3908b331846
tentatshu <tentatshu@gmail.com>
2019-09-27 12:23:17 -0700

    Adding distance function

--- swarm/cleaning.py
+++ swarm/cleaning.py
@@ -63,4 +63,88 @@ def parse_and_clean_swarm_venue_responses(check_in_response):
         .withColumn('lat_truncated', F.format_number(F.col('lat'), 2)) \
         .withColumn('lng_truncated', F.format_number(F.col('lng'), 2))
     return result
+
+
+from math import sin, cos, sqrt, atan2, radians
+from pyspark.sql.window import Window
+import pyspark.sql.types as types
+
+def calculate_distance(enhanced_checkin_data):
+    """ Create distance (km) column in PySpark dataframe 
+
+    Parameters
+    ----------
+    enhanced_checkin_data: Dataframe that has gone through parse_and_clean_swarm_venue_responses function or
+        has following equivalent columns: created_at, lat, lng
+   
+
+    Returns
+    -------
+    Same dataframe that has an additional column called distance_in_km
+    
+    Notes
+    -----
+    
+
+    """
+    # remove records that do not have geocoordinates 
+    result = enhanced_checkin_data.filter(F.col('lat').isNotNull())
+    
+    # add prior latitude and longitude as new columns for every record
+    w1 = Window.orderBy(result.createdAt.asc())
+    result = result.withColumn('prior_latitude', F.lag(F.col('lat'), 1).over(w1)) \
+        .withColumn('prior_longitude', F.lag(F.col('lng'), 1).over(w1)) \
+        .withColumn('prior_name', F.lag(F.col('name'), 1).over(w1)) \
+        .withColumn('prior_country', F.lag(F.col('country'), 1).over(w1))
+    
+    # remove the first data point for the calculation given there is no distance to be performed
+    result = result.filter(F.col('prior_latitude').isNotNull())
+
+    # calculate distance in km leveraging udf define below
+    result = result.withColumn('distance_in_km', calculate_distance_udf('lat', 'lng', 'prior_latitude', 'prior_longitude'))
+
+    return result 
+
+
+def calculate_distance_function(lat_1, long_1, lat_2, long_2):
+    """ PySpark UDF to define distance between 2 points based on latitude and longitude
+
+    Parameters
+    ----------
+    lat_1: latitude of first datapoint
+    long_1: longitude of first datapoint
+    lat_2: latitude of second datapoint
+    long_2: longitude of second datapoint
+    
+
+    Returns
+    -------
+    Distance in KM
+    
+    
+    Notes
+    -----
+    Source: https://www.movable-type.co.uk/scripts/latlong.html
+    Must be registered with udf function to utilize in PySpark.
+
+    """
+    R = 6373.0
+    
+    lat1 = radians(lat_1)
+    lon1 = radians(long_1)
+    lat2 = radians(lat_2)
+    lon2 = radians(long_2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
+# register above function as PySpark UDF
+calculate_distance_udf = F.udf(calculate_distance_function, types.FloatType())
     
